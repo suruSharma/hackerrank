442899fe04da9df55525dc0965922e67855d27cb
suruSharma <suruchi.sharma7389@gmail.com>
2015-11-13 15:22:27 -0500

    11132015

--- src/algos/strings/Anagram.java
+++ src/algos/strings/Anagram.java
@@ -1,7 +1,9 @@
 package algos.strings;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -21,39 +23,22 @@ public class Anagram {
       String s1 = input.substring(0, (len / 2));
       String s2 = input.substring((len / 2), len);
 
-      char[] s1Arr = s1.toCharArray();
-      char[] s2Arr = s2.toCharArray();
-      int count = 0;
-      Map<Character, Integer> s1Map = new HashMap<Character, Integer>();
-      Map<Character, Integer> s2Map = new HashMap<Character, Integer>();
+      List<Character> s1List = new ArrayList<Character>();
+      List<Character> s2List = new ArrayList<Character>();
 
       for (int i = 0; i < len / 2; i++) {
-        if (s1Map.containsKey(s1.charAt(i))) {
-          s1Map.put(s1.charAt(i), s1Map.get(s1.charAt(i)) + 1);
-        } else {
-          s1Map.put(s1.charAt(i), 1);
-        }
-
-        if (s2Map.containsKey(s2.charAt(i))) {
-          s2Map.put(s2.charAt(i), s2Map.get(s2.charAt(i)) + 1);
-        } else {
-          s2Map.put(s2.charAt(i), 1);
-        }
+        s1List.add(s1.charAt(i));
+        s2List.add(s2.charAt(i));
       }
 
-      for (Character c : s2Map.keySet()) {
-        int val = s2Map.get(c);
-
-        if (!s1Map.containsKey(c)) {
-          count++;
-        } else {
-          int s1Val = s1Map.get(c);
-          if (s1Val < val) {
-            count+=(val-s1Val);
-          }
+      for (int i = 0; i < s1List.size(); i++) {
+        Character ch = s1List.get(i);
+        if (s2List.contains(ch)) {
+          s2List.remove(ch);
         }
       }
-      System.out.println(count);
+      System.out.println(s2List.size());
+
       t--;
     }
   }
